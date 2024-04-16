c8566c5dc58c02435285f38ac31e69acff1e4bdd
Sir Qasim <m.qasim077@gmail.com>
2024-04-16 09:42:03 +0500

    updated

--- main.py
+++ main.py
@@ -1,35 +1,14 @@
-from time import time
-from fastapi import FastAPI, __version__
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from typing import Any
 
-app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app = FastAPI(title="HBS")
 
-html = f"""
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>FastAPI on Vercel</title>
-        <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
-    </head>
-    <body>
-        <div class="bg-gray-200 p-4 rounded-lg shadow-lg">
-            <h1>Hello from FastAPI@{__version__}</h1>
-            <ul>
-                <li><a href="/docs">/docs</a></li>
-                <li><a href="/redoc">/redoc</a></li>
-            </ul>
-            <p>Powered by <a href="https://vercel.com" target="_blank">Vercel</a></p>
-        </div>
-    </body>
-</html>
-"""
+@app.get('/')
+def index():
+    return "HBS School home function details"
 
-@app.get("/")
-async def root():
-    return HTMLResponse(html)
-
-@app.get('/ping')
-async def hello():
-    return {'res': 'pong', 'version': __version__, "time": time()}
+@app.get('/student/{id}')
+def student(id:int):
+    data :dict[int,str] = {1:'Qasim',2:'Zia Khan'}
+    found_data: dict[Any,Any] = {"id":id, "student":data[id]}
+    return found_data
